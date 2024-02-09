31847eacb007ea83502ff4a8620317ff6c007227
chilledab <126979651+chilledab@users.noreply.github.com>
2024-02-09 23:32:17 +0530

    Add files via upload

---
+++ Codef.java
@@ -0,0 +1,148 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Codef {
+    public static void main(String args[]){
+        
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            int k = sc.nextInt();
+            
+        }
+        
+        int arr[] = new int[1667];
+        int j=0;
+        for(int i=1;i<1667;i++){
+            
+            if(i%3!=0 && i%10!=3){
+                arr[j]=i;
+                j++;
+            }
+        }
+        for(int i=0;i<t;i++){
+            int k =sc.nextInt();
+            System.out.println(arr[k-1]);
+        }
+        
+        
+
+
+        
+    }
+
+}
+
+    
+
+
+
+
+
+    
+
+        
+        
+
+
+        
+
+        
+        
+
+
+
+         
+
+        
+
+    
+
+
+        
+        
+        
+
+        
+    
+    
+    
+
+
+
+
+
+        
+        
+
+    
+
+        
+
+        
+    
+ 
+
+        
+        
+        
+        
+        
+
+
+        
+
+    
+
+
+
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+        
+
+
+
+
+
+         
+        
+
+
+        
+        
+
+        
+
+
+        
+
+        
+        
+        
+
+
+        
+
+    
+
+
