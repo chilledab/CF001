7819c78c998c21ccb5834f43f6a3cea9b708da4f
chilledab <126979651+chilledab@users.noreply.github.com>
2024-02-10 11:06:24 +0530

    Add files via upload

---
+++ Codef.java
@@ -0,0 +1,211 @@
+import java.util.*;
+
+public class Codef {
+    public static void main(String args[]){
+        
+
+        // Scanner sc = new Scanner(System.in);
+        // int t = sc.nextInt();
+        // for(int i=0;i<t;i++){
+        //     int k = sc.nextInt();
+            
+        // }
+        
+        // int arr[] = new int[1667];
+        // int j=0;
+        // for(int i=1;i<1667;i++){
+            
+        //     if(i%3!=0 && i%10!=3){
+        //         arr[j]=i;
+        //         j++;
+        //     }
+        // }
+        // for(int i=0;i<t;i++){
+        //     int k =sc.nextInt();
+        //     System.out.println(arr[k-1]);
+        // }
+        
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int count=0;
+        // int arr[] = {100,20,10,5,1};
+        // for(int i=0;i<arr.length;i++){
+        //     while(n>=arr[i]){
+        //         n=n-arr[i];
+        //         count++;
+        //     }
+        // }
+        // System.out.println(count);
+
+        // Scanner sc = new Scanner(System.in);
+        // char [] c = {'a','e','i','o','u'};
+        // String[] s01 = sc.next().split(" ");
+        // String[] s02 = sc.next().split(" ");
+        // String[] s03 = sc.next().split(" ");
+        // for(char c : s01){
+
+        // }
+        // Scanner sc = new Scanner(System.in);
+        // int t = sc.nextInt();
+        // for(int z=0;z<t;z++){
+        //     int n = sc.nextInt();
+        //     int k = sc.nextInt();
+        //     int x = sc.nextInt();
+        //     int count=0;
+        //     int j=0;
+        //     int arr[] = new int[n];
+        //     for(int i=n-1;i>=0;i--){
+        //         arr[j]=i;
+        //         j++;
+        //     }
+        //     for (int i = 0; i < n; i++) {
+        //         int num = sc.nextInt();
+        //         if() {
+        //             count += num;
+        //         }
+        //     }
+            
+        //     if (count >= x) {
+        //         System.out.println("YES");
+        //     } else {
+        //         System.out.println("NO");
+        //     }
+        // }
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[4];
+        for(int i=0;i<4;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        int a = arr[3]-arr[0];
+        int b = arr[3]-arr[1];
+        int c = arr[3] - arr[2];
+    
+        System.out.println(a+" "+b+" "+c);
+        
+        
+
+        
+        
+        
+
+        
+
+
+        
+    }
+
+}
+
+    
+
+
+
+
+
+    
+
+        
+        
+
+
+        
+
+        
+        
+
+
+
+         
+
+        
+
+    
+
+
+        
+        
+        
+
+        
+    
+    
+    
+
+
+
+
+
+        
+        
+
+    
+
+        
+
+        
+    
+ 
+
+        
+        
+        
+        
+        
+
+
+        
+
+    
+
+
+
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+        
+
+
+
+
+
+         
+        
+
+
+        
+        
+
+        
+
+
+        
+
+        
+        
+        
+
+
+        
+
+    
+
+
